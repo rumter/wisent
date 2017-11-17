e72ee7193e583f62dbf837d368d13a6be7e73eee
Mitin Ilya <imitin@marathonbet.ru>
2017-11-17 18:21:06 +0300

    h2 web console

---
+++ src/main/java/ru/rumter/todolist/wisent/boot/H2ConsoleConfiguration.java
@@ -0,0 +1,18 @@
+package ru.rumter.todolist.wisent.boot;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2ConsoleConfiguration {
+
+    @Bean
+    ServletRegistrationBean h2ServletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/h2/*");
+        return registrationBean;
+    }
+
+}
